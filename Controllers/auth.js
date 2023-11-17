9bf95f8fa69c812b4980f9af690e54ff7c74377f
JoanManuel17 <110619713+JoanManuel17@users.noreply.github.com>
2023-11-16 21:15:25 -0500

    Cambios Toquen

--- Controllers/auth.js
+++ Controllers/auth.js
@@ -1,50 +1,78 @@
-const express = require('express');
-const bcrypt = require('bcryptjs');
-const Usuario = require('../models/Usuario');
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const Usuario = require("../models/Usuario");
 
-const crearUsuario = async (req, res = express.request) => { 
-    const {name, email, password} = req.body
+const crearUsuario = async (req, res = express.request) => {
+  const { name, email, password } = req.body;
 
-    try {
-        let usuario = await Usuario.findOne({email: email});
-        if (usuario) {
-            return res.status(400).json({
-                ok: false,
-                msg: 'Un usuario existe con ese correo'
-            })
-        }
+  try {
+    let usuario = await Usuario.findOne({ email: email });
+    if (usuario) {
+      return res.status(400).json({
+        ok: false,
+        msg: "Un usuario existe con ese correo",
+      });
+    }
 
-        usuario = new Usuario(req.body);
-        const salt = bcrypt.genSaltSync();
-        usuario.password = bcrypt.hashSync(password, salt);
-        await usuario.save();
+    usuario = new Usuario(req.body);
+    const salt = bcrypt.genSaltSync();
+    usuario.password = bcrypt.hashSync(password, salt);
+    await usuario.save();
 
-        res.status(200).json({
-            ok: true,
-            msg: 'Usuario Creado',
-            usuario
-        })
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            ok: false,
-            msg: 'Error al crear usuario'
-        })
+    res.status(200).json({
+      ok: true,
+      msg: "Usuario Creado",
+      usuario,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Error al crear usuario",
+    });
+  }
+};
+
+const loginUsuario = async (req, res = express.request) => {
+  const { email, password } = req.body;
+
+  try {
+    let usuario = await Usuario.findOne({ email: email });
+    if (!usuario) {
+      return res.status(400).json({
+        ok: false,
+        msg: "El usuario no existe con ese email",
+      });
+    }
+
+    const passwordValid = bcrypt.compareSync(password, usuario.password);
+    if (!passwordValid) {
+      return res.status(400).json({
+        ok: false,
+        msg: "Password incorrecto",
+      });
+    }
+
+    res.status(200).json({
+      ok: true,
+      msg: "Login",
+      usuario,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Error al crear usuario",
+    });
     }
 }
 
-const loginUsuario = (req, res = express.request) => {
-    res.json({  ok: true,
-                msg: 'Login Usuario' });
-}
-
-const revalidarToken = (req, res = express.request) => {
-    res.json({  ok: true,
-                msg: 'Renovar Token' });
-}
+  const revalidarToken = (req, res = express.request) => {
+    res.json({ ok: true, msg: "Renovar Token" });
+  };
 
 module.exports = {
-    crearUsuario,
-    loginUsuario,
-    revalidarToken
-}
+  crearUsuario,
+  loginUsuario,
+  revalidarToken,
+};
