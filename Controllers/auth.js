311bdca3d586f0f5a63f0fc3f6542bf846e2f669
JoanManuel17 <110619713+JoanManuel17@users.noreply.github.com>
2023-11-16 20:38:03 -0500

    cambios

--- Controllers/auth.js
+++ Controllers/auth.js
@@ -5,24 +5,6 @@ const Usuario = require('../models/Usuario');
 const crearUsuario = (req, res = express.request) => { 
     const {name, email, password} = req.body
 
-    try {
-        const usuario = new Usuario(req.body);
-        await usuario.save();
-
-        res.status(200).json({
-            ok: true,
-            msg: 'Usuario Creado',
-            usuario
-        })
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            ok: false,
-            msg: 'Error al crear usuario'
-        })
-    }
-
-
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({
