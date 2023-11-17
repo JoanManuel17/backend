a90c35d18d9b081cea3a929166c20669b8008f2e
JoanManuel17 <110619713+JoanManuel17@users.noreply.github.com>
2023-11-16 20:56:05 -0500

    Encryp

--- Controllers/auth.js
+++ Controllers/auth.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 const Usuario = require('../models/Usuario');
 
 const crearUsuario = async (req, res = express.request) => { 
     const {name, email, password} = req.body
 
     try {
-        const usuario = new Usuario(req.body);
+        let usuario = await Usuario.findOne({email: email});
+        if (usuario) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Un usuario existe con ese correo'
+            })
+        }
+
+        usuario = new Usuario(req.body);
+        const salt = bcrypt.genSaltSync();
+        usuario.password = bcrypt.hashSync(password, salt);
         await usuario.save();
 
         res.status(200).json({
